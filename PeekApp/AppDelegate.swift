a31f216ef606679ddcfd8248602b0886c8c24b16
Josue Franco Cadillo Costa <jfcadillo951@gmail.com>
2021-07-11 09:33:49 -0500

    first commit

--- PeekApp/AppDelegate.swift
+++ PeekApp/AppDelegate.swift
@@ -11,10 +11,14 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let queryString = "GraphQL"
+        Client.shared.apollo.fetch(
+            query: SearchRepositoriesQuery(query: queryString, count: 10)
+        ) { value in
+            debugPrint(value)
+        }
         return true
     }
 
---
+++ PeekApp/Core/Network/Client.swift
@@ -0,0 +1,27 @@
+//
+//  Client.swift
+//  PeekApp
+//
+//  Created by Josué Cadillo on 10-07-21.
+//
+
+import Foundation
+import Apollo
+
+final class Client {
+    static let shared = Client()
+    private let token = "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"
+    private let url = "https://api.github.com/graphql"
+    lazy var apollo: ApolloClient = {
+        let cache = InMemoryNormalizedCache()
+        let store = ApolloStore(cache: cache)
+        let network = RequestChainNetworkTransport(
+            interceptorProvider: DefaultInterceptorProvider(store: store),
+            endpointURL: URL(string: url)!,
+            additionalHeaders: [
+                "Authorization": "Bearer \(token)"
+            ]
+        )
+        return .init(networkTransport: network, store: store)
+    }()
+}
